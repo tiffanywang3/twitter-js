4da61848e8a99d7674512bd5574a03933c4db23e
Tiffany W <tiffany.wang777@gmail.com>
2015-09-22 17:21:02 -0400

    add welcome

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+	res.send('Welcome')
+})
+
+app.listen(3000)
+console.log('server listening')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
