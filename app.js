e42c285be99d0faaa044ef844f4c01b2260100f4
Tiffany W <tiffany.wang777@gmail.com>
2015-09-22 18:07:03 -0400

    add nodemon

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+
+app.use(function(req,res,next){
+	//do logging here
+   console.log("request", req.method, req.url, res.statusCode);
+   //console.log("response", res);
+   next();
+})
+// app.use(morgan('combined'));
+
 
 app.get('/',function(req,res){
 	res.send('Welcome')
 })
+
+app.get('/news',function(req,res){
+	res.send('This is the news page')
+})
+
+app.get('/hello',function(req,res){
+	res.send('Hello world')
+})
+
+
+
+
 
 app.listen(3000)
 console.log('server listening')
